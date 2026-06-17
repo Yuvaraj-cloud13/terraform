4e930fd18e5ce6152597de6e0e2a1084549da666
yuvaraj <yuvarajhv1311@gmail.com>
2026-06-17 16:22:53 +0530

    terraform

---
+++ 06-for-loop/main.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "roboshop" {
+  for_each = var.instances
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [
+      aws_security_group.roboshop[each.key].id,
+      aws_security_group.common.id
+  ]
+
+  tags = {
+    Name = "${var.project}-${var.environment}-${each.key}"
+  }
+  
+}
+
+resource "aws_security_group" "roboshop" {
+  for_each = var.instances
+  name        = "${var.project}-${var.environment}-${each.key}"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "-1"
+    cidr_blocks = var.cidr
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-${each.key}"
+  }
+}
+
+
+resource "aws_security_group" "common" {
+  name        = "${var.project}-${var.environment}-common"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "-1"
+    cidr_blocks = var.cidr
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-common"
+  }
+}
---
+++ 06-for-loop/variable.tf
@@ -0,0 +1,94 @@
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "project" {
+  type = string
+  default = "roboshop"
+}
+
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "RHEL9 joindevops image"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium", "t3.large"], var.instance_type)
+    error_message = "instance type should be either t3.micro or t3.small"
+  }
+
+}
+
+
+
+variable "sg_name" {
+  type    = string
+  default = "allow_terraform_vars"
+}
+
+
+variable "port" {
+  type    = number
+  default = 0
+
+}
+
+
+variable "cidr" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "instances" {
+  type = map
+  default =  {
+    mongodb = {
+      "instance_type" = "t3.micro"
+    },
+    redis = {
+      "instance_type" = "t3.micro"
+    },
+    mysql = {
+      "instance_type" = "t3.micro"
+    },
+    rabbitmq = {
+      "instance_type" = "t3.micro"
+    },
+    catalogue = {
+      "instance_type" = "t3.micro"
+    },
+    user = {
+      "instance_type" = "t3.micro"
+    },
+    cart = {
+      "instance_type" = "t3.micro"
+    },
+    shipping = {
+      "instance_type" = "t3.micro"
+    },
+    payment = {
+      "instance_type" = "t3.micro"
+    },
+    frontend = {
+      "instance_type" = "t3.micro"
+    }
+  
+  }
+}
+
+
+variable "zone_id" {
+  type = string
+  default = "Z00746892H2TX4JV63UWM"
+}
+
+variable "domine_name" {
+  default = "uvraj.online"
+}
