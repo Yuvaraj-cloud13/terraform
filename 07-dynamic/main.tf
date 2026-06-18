4694ff488007e6d03a9a101daf3307d8a2dd956d
yuvaraj <yuvarajhv1311@gmail.com>
2026-06-18 18:15:16 +0530

    terraform

---
+++ 07-dynamic/main.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "terraform_demo" {
+
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_terraform.id]
+
+  tags = {
+    Name = "terraform-demo-1"
+    Project = "roboshop"
+    Environment = "dev"
+}
+
+}
+
+resource "aws_security_group" "allow_terraform" {
+  name        = "allow_terraform_dynamic"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  dynamic "ingress" {
+  for_each = var.ingress_rules
+    content {
+      from_port        = ingress.value.port
+      to_port          = ingress.value.port
+      protocol         = "tcp"
+      cidr_blocks      = ingress.value.cidr_blocks
+    }
+
+  }
+
+  
+  tags = {
+    Name = "allow_terraform"
+    Project = "roboshop"
+    Environment = "dev"
+  }
+}
