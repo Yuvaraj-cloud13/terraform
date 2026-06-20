ba6212017dec2f4dc20cc75b5934515f9e7eab88
yuvaraj <yuvarajhv1311@gmail.com>
2026-06-20 19:56:05 +0530

    terraform

---
+++ 11-provisioners/main.tf
@@ -0,0 +1,79 @@
+resource "aws_instance" "terraform_demo" {
+
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_terraform.id]
+
+  tags = {
+    Name = "terraform-demo-1"
+    Project = "roboshop"
+    Environment = "dev"
+}
+
+
+  provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.private_ip} > inventory.ini"
+  }
+
+
+  provisioner "local-exec" {
+    command = "echo instance created"
+  }
+
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "echo instance is going to destroy"
+  }
+
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "echo > inventory.ini"
+  }
+
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = self.public_ip
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install nginx -y",
+    ]
+  }
+
+
+
+
+}
+
+resource "aws_security_group" "allow_terraform" {
+  name        = "allow_terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "allow_terraform"
+    Project = "roboshop"
+    Environment = "dev"
+  }
+}
